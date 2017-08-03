b9b47cc20a9b0c7e6cf39be3ccdd26294775b051
luoweiqiang <763231564@qq.com>
2017-08-03 15:43:14 +0800

    integrate spring mvc

---
+++ src/main/java/com/lovech/dynamicsite/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.lovech.dynamicsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by luowq on 2017/8/3.
+ */
+@Controller
+public class IndexController {
+
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate );
+
+        return "index";
+    }
+}
